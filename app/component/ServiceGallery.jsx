f96621363f1ec7cbfaf590a2272a46c2ee824115
Sharif <163577738+sharifstack@users.noreply.github.com>
2025-01-11 23:22:31 +0600

    Add files via upload

--- app/component/ServiceGallery.jsx
+++ app/component/ServiceGallery.jsx
@@ -14,46 +14,41 @@ import cardHover from "../assets/cardHover.png";
 import cardActive from "../assets/cardActive.png";
 import cardBg from "../assets/cardBg.png";
 
-const ServiceGallery = () => {
+function ServiceGallery() {
   let services = [
     { name: "Graphics Design", icon: GD, activeIcon: GDActive },
     { name: "Web Developer", icon: WD, activeIcon: WDActive },
     { name: "Digital Marketing", icon: DM, activeIcon: DMActive },
-    { name: "App Design", icon: AD, activeIcon: ADActive },
+    { name: "App Design",  icon: AD, activeIcon: ADActive },
   ];
 
   return (
     <div>
       <Container className="grid sm:grid-cols-2 g 2xl:grid-cols-4 gap-10 2xl:gap-x-16 pt-20 overflow-x-hidden scrollbar">
-        {services.map((service, index) => (
+        {services.map((service) => (
           <div className="card  relative z-10 group   ">
             <Image
               className="absolute group-hover:-rotate-12 duration-300 h-full w-full top-0 left-0 -z-10"
               src={cardHover}
-              alt="card"
-            />
+              alt="card" />
             <Image
               className="absolute h-full w-full top-0 left-0 -z-10 opacity-0 group-hover:opacity-100 "
               src={cardActive}
-              alt="card"
-            />
+              alt="card" />
             <Image
               className="absolute h-full w-full top-0 left-0 -z-10 opacity-100 group-hover:opacity-0 !drop-shadow-lg "
               src={cardBg}
-              alt="card"
-            />
+              alt="card" />
             <div className="main py-16 2xl:py-20 px-6 2xl:px-8 flex flex-col items-center gap-y-12">
               <div className="icon aspect-square max-w-24">
                 <Image
                   src={service.icon}
                   alt="assets/digitalMarketingIcon.png"
-                  className="group-hover:hidden duration-300 h-full w-full"
-                />
+                  className="group-hover:hidden duration-300 h-full w-full" />
                 <Image
                   src={service.activeIcon}
                   alt="assets/digitalMarketingIcon.png"
-                  className=" hidden group-hover:flex duration-300 h-full w-full"
-                />
+                  className=" hidden group-hover:flex duration-300 h-full w-full" />
               </div>
               <div className="text flex flex-col text-center gap-y-5">
                 <h2 className="text-2xl font-bold font-nunito text-black">
@@ -70,6 +65,6 @@ const ServiceGallery = () => {
       </Container>
     </div>
   );
-};
+}
 
 export default ServiceGallery;
